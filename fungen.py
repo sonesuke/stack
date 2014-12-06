2693af00cfc0fe7ddda59a7e9d7832749ff7b800
sonesuke <iamsonesuke@gmail.com>
2014-12-06 20:35:19 +0900

    pep 8

--- fungen.py
+++ fungen.py
@@ -4,40 +4,60 @@ from pyparsing import *
 name = Word(alphanums)
 type_string = Word(alphas)
 
+
 class Arg(object):
+
     def __init__(self, name, type_string):
         self.name = name
         self.type_string = type_string
 
-arg = name.setResultsName('Name') + Literal(':') + type_string.setResultsName('Type') + Literal(';')
+arg = name.setResultsName('Name')
+arg += Literal(':')
+arg += type_string.setResultsName('Type') + Literal(';')
 arg.setParseAction(lambda ts: Arg(ts['Name'], ts['Type']))
 
+
 class ArgsInput(object):
+
     def __init__(self, args):
         self.args = args
 
-args_input = CaselessKeyword('var_input') + Group(OneOrMore(arg)).setResultsName('Args') + CaselessKeyword('end_var')
+args_input = CaselessKeyword('var_input')
+args_input += Group(OneOrMore(arg)).setResultsName('Args')
+args_input += CaselessKeyword('end_var')
 args_input.setParseAction(lambda ts: ArgsInput(ts['Args']))
 
 
 class ArgsLocal(object):
+
     def __init__(self, args):
         self.args = args
 
-args_local = CaselessKeyword('var') + Group(OneOrMore(arg)).setResultsName('Args') + CaselessKeyword('end_var')
+args_local = CaselessKeyword('var')
+args_local += Group(OneOrMore(arg)).setResultsName('Args')
+args_local += CaselessKeyword('end_var')
 args_local.setParseAction(lambda ts: ArgsLocal(ts['Args']))
 
 
 class Function(object):
-    def __init__(self, name, type_string):
+
+    def __init__(self, name, type_string, local):
         self.name = name
         self.ret_type = type_string
+        self.args_local = local
 
-function = CaselessKeyword('function') + name.setResultsName('Name') + Literal(':') + type_string.setResultsName('Type')
+function = CaselessKeyword('function')
+function += name.setResultsName('Name')
+function += Literal(':').setResultsName('Local')
+function += type_string.setResultsName('Type')
+function += Optional(args_local)
 function += CaselessKeyword('end_function')
-function.setParseAction(lambda ts: Function(ts['Name'], ts['Type']))
+function.setParseAction(
+    lambda ts: Function(ts['Name'], ts['Type'], ts['Local']))
+
 
 class Parser(object):
+
     def __init__(self):
         pass
 
--- test_fungen.py
+++ test_fungen.py
@@ -1,5 +1,15 @@
 from fungen import *
 
+import pep8
+import glob
+
+
+def test_pep8():
+    pep8style = pep8.StyleGuide()
+    result = pep8style.check_files(glob.glob('*.py'))
+    assert result.total_errors == 0
+
+
 """
 function B: void
     var input
@@ -28,61 +38,63 @@ function A: void
 
 end_function
 """
+
+
 def test_arg():
-      target = "a1: WORD;"
-      p = Parser()
-      a = p.arg(target)
-      assert isinstance(a, Arg)
-      assert a.name == 'a1'
-      assert a.type_string == 'WORD'
+    target = "a1: WORD;"
+    p = Parser()
+    a = p.arg(target)
+    assert isinstance(a, Arg)
+    assert a.name == 'a1'
+    assert a.type_string == 'WORD'
 
 
 def test_input_args():
-      target = """
+    target = """
       var_input
           a0: WORD;
           a1: DWORD;
       end_var
       """
-      p = Parser()
-      a = p.args_input(target)
-      assert isinstance(a, ArgsInput)
-      assert a.args[0].name == 'a0'
-      assert a.args[0].type_string == 'WORD'
-      assert a.args[1].name == 'a1'
-      assert a.args[1].type_string == 'DWORD'
+    p = Parser()
+    a = p.args_input(target)
+    assert isinstance(a, ArgsInput)
+    assert a.args[0].name == 'a0'
+    assert a.args[0].type_string == 'WORD'
+    assert a.args[1].name == 'a1'
+    assert a.args[1].type_string == 'DWORD'
 
 
 def test_local_args():
-      target = """
+    target = """
       var
           a0: WORD;
           a1: DWORD;
       end_var
       """
-      p = Parser()
-      a = p.args_local(target)
-      assert isinstance(a, ArgsLocal)
-      assert a.args[0].name == 'a0'
-      assert a.args[0].type_string == 'WORD'
-      assert a.args[1].name == 'a1'
-      assert a.args[1].type_string == 'DWORD'
+    p = Parser()
+    a = p.args_local(target)
+    assert isinstance(a, ArgsLocal)
+    assert a.args[0].name == 'a0'
+    assert a.args[0].type_string == 'WORD'
+    assert a.args[1].name == 'a1'
+    assert a.args[1].type_string == 'DWORD'
 
 
 def test_function():
-      target = """
+    target = """
       function A: void
       end_function
       """
-      p = Parser()
-      a = p.function(target)
-      assert isinstance(a, Function)
-      assert a.name == 'A'
-      assert a.ret_type == 'void'
+    p = Parser()
+    a = p.function(target)
+    assert isinstance(a, Function)
+    assert a.name == 'A'
+    assert a.ret_type == 'void'
 
 
 def test_function_with_local():
-      target = """
+    target = """
       function A: void
       var
           a0: WORD;
@@ -90,8 +102,8 @@ def test_function_with_local():
       end_var
       end_function
       """
-      p = Parser()
-      a = p.function(target)
-      assert isinstance(a, Function)
-      assert a.name == 'A'
-      assert a.ret_type == 'void'
+    p = Parser()
+    a = p.function(target)
+    assert isinstance(a, Function)
+    assert a.name == 'A'
+    assert a.ret_type == 'void'
